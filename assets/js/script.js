3a0ae4a1f26b7c95b38193a5d5ffc793a44ab5ba
Jay Cohen <49128160+jay-cohen@users.noreply.github.com>
2021-01-07 15:54:21 +0000

    Google Analytics code added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function ($) {
     
-    var count = 1;  
+  var count = 1;  
 	var slideCount = $('.slider ul li').length;
 	var slideWidth = $('.slider ul li').width();
 	var slideHeight = $('.slider ul li').height();
@@ -8,8 +8,8 @@ jQuery(document).ready(function ($) {
 	
 	$('.slider').css({ width: slideWidth, height: slideHeight });
 	$('.slider ul').css({ width: sliderUlWidth, marginLeft: - slideWidth });
-    $('.slider ul li:last-child').prependTo('.slider ul');
-    $('.total').html('/'+slideCount);
+  $('.slider ul li:last-child').prependTo('.slider ul');
+  $('.total').html('/'+slideCount);
 
     function moveLeft() {
         $('.slider ul').animate({
@@ -66,7 +66,7 @@ window.addEventListener("DOMContentLoaded", function() {
 
     // get the form elements defined in your form HTML above
     
-    var form = document.getElementById("contact-form");
+    var form   = document.getElementById("contact-form");
     var button = document.getElementById("submit-btn");
     var status = document.getElementById("submit-status");
 
@@ -84,12 +84,13 @@ window.addEventListener("DOMContentLoaded", function() {
 
     // handle the form submission event
 
-    form.addEventListener("submit", function(ev) {
-      ev.preventDefault();
-      var data = new FormData(form);
-      ajax(form.method, form.action, data, success, error);
-    });
-  });
+    if(form) {
+      form.addEventListener("submit", function(ev) {
+        ev.preventDefault();
+        var data = new FormData(form);
+        ajax(form.method, form.action, data, success, error);
+      });
+    }
   
   // helper function for sending an AJAX request
 
@@ -107,3 +108,5 @@ window.addEventListener("DOMContentLoaded", function() {
     };
     xhr.send(data);
   }
+
+});
